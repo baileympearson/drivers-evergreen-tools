b31ca92776da868e71fba357e03d117c23df0e9b
bailey <bailey.pearson@mongodb.com>
2025-04-04 11:51:15 -0600

    logs

--- .evergreen/csfle/start-servers.sh
+++ .evergreen/csfle/start-servers.sh
@@ -7,15 +7,15 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 pushd $SCRIPT_DIR
 
 if [ ! -f ./secrets-export.sh ]; then
-    echo "Please run the setup-secrets.sh script"
-    exit 1
+  echo "Please run the setup-secrets.sh script"
+  exit 1
 fi
 
 source ./secrets-export.sh
 
 if [ -z "${CSFLE_TLS_CA_FILE-}" ]; then
-    echo "Please run the setup-secrets.sh script"
-    exit 1
+  echo "Please run the setup-secrets.sh script"
+  exit 1
 fi
 
 . ./stop-servers.sh
@@ -38,49 +38,44 @@ if [ "$(uname -s)" != "Darwin" ]; then
   COMMAND="nohup $COMMAND"
 fi
 
-
 echo "Starting KMIP Server..."
 # TMPDIR is required to avoid "AF_UNIX path too long" errors.
-TMPDIR="$(dirname "$DRIVERS_TOOLS")" $COMMAND kms_kmip_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file $CSFLE_TLS_CERT_FILE --port 5698 > kms_kmip_server.log 2>&1 &
-echo "$!" > kmip_pids.pid
+TMPDIR="$(dirname "$DRIVERS_TOOLS")" $COMMAND kms_kmip_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file $CSFLE_TLS_CERT_FILE --port 5698 >kms_kmip_server.log 2>&1 &
+echo "$!" >kmip_pids.pid
 sleep 1
 cat kms_kmip_server.log
 echo "Starting KMIP Server...done."
 
-
 echo "Starting HTTP Server 1..."
-$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file ../x509gen/expired.pem --port 9000 > http1.log 2>&1 &
-echo "$!" >> kmip_pids.pid
+$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file ../x509gen/expired.pem --port 9000 >http1.log 2>&1 &
+echo "$!" >>kmip_pids.pid
 sleep 1
 cat http1.log
 echo "Starting HTTP Server 1...done."
 
-
 echo "Starting HTTP Server 2..."
-$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file ../x509gen/wrong-host.pem --port 9001 > http2.log 2>&1 &
-echo "$!" >> kmip_pids.pid
+$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file ../x509gen/wrong-host.pem --port 9001 >http2.log 2>&1 &
+echo "$!" >>kmip_pids.pid
 sleep 1
 cat http2.log
 echo "Starting HTTP Server 2...done."
 
-
 echo "Starting HTTP Server 3..."
-$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file $CSFLE_TLS_CERT_FILE --port 9002 --require_client_cert > http3.log 2>&1 &
-echo "$!" >> kmip_pids.pid
+$COMMAND kms_http_server.py --ca_file $CSFLE_TLS_CA_FILE --cert_file $CSFLE_TLS_CERT_FILE --port 9002 --require_client_cert &
+echo "$!" >>kmip_pids.pid
 sleep 1
-cat http3.log
+# cat http3.log
 echo "Starting HTTP Server 3...done."
 
-
 echo "Starting Failpoint Server..."
-$COMMAND kms_failpoint_server.py --port 9003 > failpoint.log 2>&1 &
-echo "$!" >> kmip_pids.pid
+$COMMAND kms_failpoint_server.py --port 9003 >failpoint.log 2>&1 &
+echo "$!" >>kmip_pids.pid
 echo "Starting Failpoint Server...done."
 sleep 1
 
 echo "Starting Fake Azure IMDS..."
-$COMMAND bottle.py fake_azure:imds > fake_azure.log 2>&1 &
-echo "$!" >> kmip_pids.pid
+$COMMAND bottle.py fake_azure:imds >fake_azure.log 2>&1 &
+echo "$!" >>kmip_pids.pid
 sleep 1
 cat fake_azure.log
 echo "Starting Fake Azure IMDS...done."
